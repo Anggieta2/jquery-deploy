a7d9ee697816b308fc55d16d38b348d21ae31308
Zakaria-cpu301 <ramadanzaka27@gmail.com>
2025-07-10 10:00:49 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+$("button").click(function(){
+            $.get("https://lms.sangnilaindonesia.com/api/highlighted-portfolio", function(data, status){
+                console.log(data);
+            });
+        });
