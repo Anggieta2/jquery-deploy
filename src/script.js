ff91de5ec6b1d25297e259bf1df878ef61cf6e9e
Arinkaa2 <zakadikagg@gmail.com>
2025-07-13 23:06:23 +0700

    update path nav and side bar

--- src/script.js
+++
@@ -1,6 +0,0 @@
-$("button").click(function(){
-            $.get("https://lms.sangnilaindonesia.com/api/highlighted-portfolio", function(data, status){
-                console.log(data);
-            });
-        });
-
